0da0b7e8bd940e4ee7fc149ae174c4a3088a1fc1
Edwin Vautier <edwin.vautier@hetic.net>
2020-09-19 11:56:56 +0200

    Feat: Initialized project with prompt for name and modules

---
+++ README.md
@@ -0,0 +1,3 @@
+# Go project CLI
+
+A CLI to initialize go projects.
---
+++ cmd/create.go
@@ -0,0 +1,81 @@
+/*
+Copyright © 2020 Edwin Vautier edwin.vautier@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	log "github.com/sirupsen/logrus"
+	"strings"
+	"github.com/spf13/cobra"
+	"github.com/AlecAivazis/survey"
+)
+
+// createCmd represents the create command
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "This command is used to initialize a new application.",
+	Long: `This command is used to initialize a new application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		appName := strings.Join(args, "-")
+
+		if len(args) != 1 {
+			userRefused := promptUserForAppName(appName)
+
+			if userRefused {
+				return
+			}
+		}
+
+		promptUserForModules()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(createCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func promptUserForAppName(appName string) bool{
+	log.Warn("1 parameter expected: application name.")
+			
+	answer := true
+	prompt := &survey.Confirm{
+		Message: "Do you want to use this name: " + appName + " ?",
+		Default: true,
+	}
+	survey.AskOne(prompt, &answer)
+	
+	if answer == false {
+		return true
+	}
+	return false
+}
+func promptUserForModules() {
+	modules := []string{}
+	prompt := &survey.MultiSelect{
+    	Message: "What modules do you want:",
+    	Options: []string{"Router", "Database", "Models"},
+	}
+	survey.AskOne(prompt, &modules)
+}
